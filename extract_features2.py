46b7898faa4a6ff2661213a2aa59b79d03676b92
jjperryj <146219540+jjperryj@users.noreply.github.com>
2024-03-18 09:34:59 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ extract_features2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy import stats
+import test_data
+
+def make_features(sensor, samples):
+    #np 1D array of fft
+    features = np.empty(4)
+
+    S = np.abs(sensor**2)/samples
+
+    #Mean
+    features[0] = np.mean(S)
+
+    #variance
+    features[1] = np.var(S)
+
+    #skewness
+    features[2] = stats.skew(S)
+
+    #kurtosis
+    features[3] = stats.kurtosis(S)
+
+    return features
+
+print(make_features(test_data.get_data(), 2000))
