f4108d0840543f2f873929535428feb345276e97
twmkok <93989715+twmkok@users.noreply.github.com>
2024-03-21 11:01:38 +0100

    Finished PCA

--- PCA.py
+++ PCA.py
@@ -13,14 +13,15 @@ labels = np.reshape(breast_labels, (569,1))
 
 final_breast_data = np.concatenate([breast_data, labels], axis=1)
 breast_dataset = pd.DataFrame(final_breast_data)
+print(final_breast_data)
 
-def PCA(X, num_components):
+def PCA(X, n):
 
     #1. Finding normalized matrix
-
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
     #2. Finding covariance matrix
-    covX = np.cov(X, rowvar=False)
-    print(pd.DataFrame.cov())
+    covX = np.cov(X_scaled, rowvar=False)
     #3. Finding eigenvectors+values
     eigenvalues,  eigenvectors = np.linalg.eig(covX)
 
@@ -29,9 +30,20 @@ def PCA(X, num_components):
     eigenvalues = eigenvalues[idx]
     eigenvectors = eigenvectors[:,idx]
     print("------")
-    PC = eigenvectors[:num_components]
-
+    S = np.diag(np.sqrt(eigenvalues))
+    S_inv = np.linalg.inv(S)
+    V = np.transpose(eigenvectors)
+    V_T = eigenvectors
+    U = np.matmul(X_scaled, V)
+    U = np.matmul(U, S_inv) 
     #5. Transform original data
-    X_transf = PC.fit_transform(X)
-
-PCA(final_breast_data,2)
+    A = np.matmul(U, S)
+    A = np.matmul(A, V_T)
+    U_t = U[:, :n]
+    S_t = S[:n, :n]
+    V_t = V[:, :n]
+    PC = np.matmul(U_t, S_t)
+    PC = np.matmul(PC, np.transpose(V_t))
+    X_transf = scaler.inverse_transform(PC)
+    return X_transf,S_t, V_t
+print(PCA(final_breast_data, 10)[0])
