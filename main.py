3ef6b6f61d316c1e231150700159bd64c7900167
g.konstantinou@student.tudelft.nl <g.konstantinou@student.tudelft.nl>
2024-03-21 14:07:53 +0100

    Made a lot of changes and fixed bugs. This shouldn't need many more changes, probably final

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ pd.set_option('display.width', 400)
 np.set_printoptions(linewidth=400)
 
 
-def list_files(dir):
+def matToCsv(dir):
     csv_dir = dir.replace('PZT','PZT-CSV')
     #     dir2 = dir
     #     csv_dir = dir2.replace('PZT', 'PZT-CSV')
@@ -19,71 +19,19 @@ def list_files(dir):
     # Create the output directory if it doesn't exist
     if not os.path.exists(csv_dir):
         os.makedirs(csv_dir)
-    #
+
+    #Iterates over all the frequency files in the directory
     for root, dirs, files in os.walk(dir):
          for name in dirs:
              if name.endswith('cycles'):
-                print(name)
-                root2 = root
-                root_new = root2.replace('PZT', 'PZT-CSV')
+                root_new = root.replace('PZT', 'PZT-CSV')
+
                 if not os.path.exists(root_new):
                     os.makedirs(root_new)
                 arrayfile = combine_act(os.path.join(root,name))
-               # print(arrayfile[0])
-                #flatlist = [item for sublist1 in arrayfile for item in sublist1]
-                #print(flatlist)
-                makeCSV(arrayfile[0],arrayfile[1])
 
-                csv_file_path = os.path.join(root_new, f"{name.replace('.mat', '')}_{"test2"}.csv")
-                arrayfile[0].to_csv(csv_file_path, index=False)
-
-    # for freq in state:
-    #     combine_act(freq)
-
-
-
-# for filename in files:
-#     if filename.endswith('rep_1.mat'):
-#
-#
-
-
-#
-#     for root, dirs, files in os.walk(dir):
-#         for name in files:
-#             if name.endswith('rep_1.mat'):
-#                 mat_array = scipy.io.loadmat(os.path.join(root, name))
-#
-#                 root2 = root
-#                 root_new = root2.replace('PZT', 'PZT-CSV')
-#                 if not os.path.exists(root_new):
-#                     os.makedirs(root_new)
-#
-#                 for key, value in mat_array.items():
-#                     if not key.startswith('_') and isinstance(value, (list, tuple, np.ndarray)):
-#                         csv_file_path = os.path.join(root_new, f"{name.replace('.mat', '')}_{key}.csv")
-#                         print(f"Creating {csv_file_path}")
-#                         #chanbe
-#
-#                         # Ensure the array is 1D
-#                         flattened_data = np.ravel(value)
-#
-#                         # Check if the length is divisible by 9
-#                         if len(flattened_data) % 9 == 0:
-#                             # Reshape the array into 9 columns
-#                             reshaped_data = np.reshape(flattened_data, (-1, 9))
-#
-#                             # Create a DataFrame with 9 columns
-#                             data = pd.DataFrame(reshaped_data, columns=[f'Column_{i + 1}' for i in range(9)])
-#
-#                             # Write the DataFrame to CSV
-#                             data.to_csv(csv_file_path, index=False)
-#                         elif (key == "Trigger_Info"):
-#                             data = pd.DataFrame(flattened_data)
-#                             data.to_csv(csv_file_path, index=False)
-#                         else:
-#                             pass
-#                         print(f"Data format invalid for key {key}. Skipping.")
+                csv_file_path = os.path.join(root_new, f"{name.replace('.mat', '')}_{'finalTest'}.csv")
+                arrayfile.to_csv(csv_file_path, index=False)
 
 
 def combine_act(freq):
@@ -96,10 +44,8 @@ def combine_act(freq):
                 mat_array = scipy.io.loadmat(os.path.join(root, name))
 
 
-
-                for key, value in mat_array.items():
+                for key, value in mat_array.items():   #Iterating over the different keys(TriggerInfo and Data files)
                      if not key.startswith('_') and isinstance(value, (list, tuple, np.ndarray)):
-                         #chanbe
 
                          # Ensure the array is 1D
                          flattened_data = np.ravel(value)
@@ -111,25 +57,19 @@ def combine_act(freq):
 
                              # Create a DataFrame with 9 columns
                              data = pd.DataFrame(reshaped_data, columns=[f'Column_{i + 1}' for i in range(9)])
-                             freq_arr.insert(actuatorNum-1,data)
-                             # Write the DataFrame to CSV
-                             # data.to_csv(csv_file_path, index=False)
-                             #print("DONE")
+
+                             freq_arr.insert(actuatorNum-1,data)  #insterts data in the correct orden (in terms of which sensor acts as actuator) into a list
+
                          elif (key == "Trigger_Info"):
                              data = pd.DataFrame(flattened_data)
                              #data.to_csv(csv_file_path, index=False)
                          else:
                             pass
                             print(f"Data format invalid for key {key}. Skipping.")
-    return freq_arr
-
-
-def makeCSV(list1,list2):
-    p.isave_as(array=(a+b for a,b in zip(list1,list2)), dest_file_name='mergeddd.csv')
-    p.free_resources()
-    print("doine")
+                combined_df = pd.concat(freq_arr, axis=1) #combines all the entries in the list into a single dataframe
+    return combined_df
 
 
 
-list_files(r"C:\Users\edlyn\Desktop\PZT-L1-03") #This should be the directory leading up to the main PZT file.
-#print(combine_act(r"C:\Users\edlyn\Desktop\PZT-L1-03\L103_2019_12_06_14_02_38\State_1_2019_12_06_14_02_38\50kHz_5cycles"))
+matToCsv(r"C:\Users\geort\Desktop\Universty\PZT-L1-03") #This should be the directory leading up to the main PZT file.
+
