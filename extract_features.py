bc36102d58fc63c6dac2f3fab949326ba85c9729
Vergouwen <c.e.p.m.vergouwen@student.tudelft.nl>
2024-03-21 09:31:12 +0100

    M

--- extract_features.py
+++ extract_features.py
@@ -23,3 +23,20 @@ def make_features(sensor, samples):
     return features
 
 #print(make_features(test_data.get_data(), 2000))
+
+
+#Time domain
+def Time_domain_features(sensor):
+    # np 1D array of time domain data
+    features = np.empty(2)
+
+    # Mean
+    features[0] = np.mean(sensor)
+
+    # Standard deviation
+    features[1] = np.std(sensor)
+
+    #RMS
+    Rms.append(np.sqrt(np.mean(X ** 2)))
+
+    return features
