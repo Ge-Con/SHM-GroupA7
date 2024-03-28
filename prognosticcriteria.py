3b9df64f083703dc716d34cde8bc11481ac01315
Dario Argentieri <150810387+argodario77@users.noreply.github.com>
2024-03-28 12:14:02 +0100

    Update prognosticcriteria.py
    Added Trendability function and imported relevant modules

--- prognosticcriteria.py
+++ prognosticcriteria.py
@@ -1,4 +1,8 @@
 import numpy as np
+import math
+from sklearn.preprocessing import Normalizer
+from scipy.stats import pearsonr
+from scipy.signal import resample_poly
 
 def Pr(X):
 
@@ -14,6 +18,48 @@ def Pr(X):
     prognosability = np.exp(-np.std(top)/np.mean(bottom))
 
     return prognosability
+
+
+def Tr(X):
+    """
+    Shape of 'X':  (m rows x n columns) where X is samples vs measurements array for a specific PC
+    where m is the number of samples
+    and n is the number of measurements (ie. cycles)
+    """
+    m, n = X.shape  # m = rows (ie. # of samples), n = columns (ie. # of cycles)
+    trendability_feature = np.inf
+    trendability_list = []
+
+    # Finding normalised matrix
+    scaler = Normalizer()
+    X = scaler.fit_transform(X)
+
+    for j in range(m):
+        # Obtain (pre-normalised) first vector of measurements
+        vector1 = X[j]
+
+        for k in range(m):
+            # Obtain (pre-normalised) second vector of measurements
+            vector2 = X[k]
+
+            # Check whether two vectors are the same length, ie. if they both experience failure at same cycle
+            # If vectors are not same length, reshape/resample them to equal lengths
+            if len(vector2) != len(vector1):
+                if len(vector2) < len(vector1):
+                    vector2 = resample_poly(vector2, len(vector1), len(vector2), window=('kaiser', 5))
+                else:
+                    vector1 = resample_poly(vector1, len(vector2), len(vector1), window=('kaiser', 5))
+
+            rho = pearsonr(vector1, vector2)[0]
+            if math.fabs(rho) < trendability_feature:  # ie. if less than infinity, give new value
+                trendability_feature = math.fabs(rho)
+
+            # Add math.fabs(rho) to list
+            trendability_list.append(trendability_feature)
+
+    # Return minimum value
+    return min(trendability_list)
+
 
 def fitness(X, Mo_a=1,Tr_b=1,Pr_c=1):
 
