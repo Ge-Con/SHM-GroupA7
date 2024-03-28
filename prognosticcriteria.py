09535c324b6dbba37d5adb91ab50583c29592993
pablo <p.garcia-condeortiz-1@student.tudelft.nl>
2024-03-28 11:53:35 +0100

    prognosability

--- prognosticcriteria.py
+++ prognosticcriteria.py
@@ -1,5 +1,16 @@
 import numpy as np
 
 def Pr(X):
+
+    M = len(X)
+    Nfeatures = X.shape[1]
+    top = np.zeros((M,Nfeatures))
+    bottom = np.zeros((M,Nfeatures))
+
     for j in range(M):
-        Pr = np.exp(
+        top[j,:] = X[j,-1]
+        bottom[j,:] = np.abs(X[j,0]-X[j,-1])
+
+    prognosability = np.exp(-np.std(top)/np.mean(bottom))
+
+    return prognosability
