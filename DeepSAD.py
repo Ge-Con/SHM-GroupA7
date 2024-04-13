e5a3569ba14c1c0581718628be23e6d3b151062d
jjperryj <146219540+jjperryj@users.noreply.github.com>
2024-04-13 17:13:30 +0200

    Added comments

--- DeepSAD.py
+++ DeepSAD.py
@@ -1,7 +1,8 @@
 import torch
 from torch import nn
 
-class model(nn.Module):
+class DeepSAD_net(nn.Module):
+    #Object for the neural network model for DeepSAD
     #mnist_LeNet implementation - this is phi in the equations
 
     def __init__(self, rep_dim=64):
@@ -26,18 +27,30 @@ class model(nn.Module):
         x = self.fc1(x)
         return x
 
+
 def init_c(model, train_data, eps=0.1):
-    """Initialize hypersphere center c as the mean from an initial forward pass on the data."""
+    """
+        Initialise hypersphere center c as the mean from an initial forward pass on the data
+
+        Parameters:
+        - model (DeepSAD_net object): Untrained DeepSAD model
+        - train_data (2D array): Training data
+        - eps (float): Hyperparameter
+
+        Returns:
+        - c (1D array): Coordinates of hypersphere centre
+    """
+
     n_samples = 0
     c = torch.zeros(model.rep_dim)
 
+    #Forward pass
     model.eval()
     with torch.no_grad():
         for data in train_data:
             outputs = model(data)
             n_samples += outputs.shape[0]
             c += torch.sum(outputs, dim=0)
-
     c /= n_samples
 
     # If c_i is too close to 0, set to +-eps. Reason: a zero unit can be trivially matched with zero weights.
@@ -46,8 +59,27 @@ def init_c(model, train_data, eps=0.1):
 
     return c
 
-def train(model, train_data, semi_targets, learning_rate, weight_decay, lr_milestones, n_epochs, gamma, eta, eps, reg=0):
-    #reg is 0 if no diversification
+
+def train(model, train_data, semi_targets, learning_rate, weight_decay, n_epochs, lr_milestones, gamma, eta, eps, reg=0):
+    """
+        Train the DeepSAD model from a semi-labelled dataset.
+
+        Parameters:
+        - model (DeepSAD_net object): DeepSAD model
+        - train_data (2D array): Training data
+        - semi_targets (yet to be implemented): Data labels
+        - learning_rate (float): Learning rate
+        - weight_decay (float): Weight decay for Adam optimizer
+        - n_epochs (int): Number of epochs
+        - lr_milestoned (int): Milestones for learning rate updates
+        - gamma (float): Gamma
+        - eta (float): eta
+        - eps (float): eps <^- hyperparameters
+        - reg(int): Lambda, weight of diversification in loss function (zero if none)
+
+        Returns:
+        None
+    """
 
     optimizer = optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=lr_milestones, gamma=gamma)
@@ -86,8 +118,23 @@ def train(model, train_data, semi_targets, learning_rate, weight_decay, lr_miles
 
         print("Epoch " + str(epoch) + ", loss = " + str(epoch_loss/n_batches))
 
-def embed(self, X, model):
-    #Get Health Indicator
+def embed(X, model):
+    """
+        Returns a health indicator for a test dataset
+
+        Parameters:
+        - X (2D array): Array containing feature data
+        - model (DeepSAD_net object): Trained DeepSAD model
+
+        Returns:
+        - y (float): Anomaly score to be used as a Health Indicator
+
+        Example:
+
+        # Example usage of the function
+        HI = Time_domain_features(feature_data, model)
+    """
+
     model.eval()
     y = torch.norm(model(X) - model.c)   #Magnitude of the vector = anomaly score
     return y
