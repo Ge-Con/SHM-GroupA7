9002ae30dd48922967eb55b5f36cb76072739826
mpjetri <m.pjetri@student.tudelft.nl>
2024-05-27 12:00:29 +0200

    changed emd form sift to just emd

--- Signal_Processing/hilbert.py
+++ Signal_Processing/hilbert.py
@@ -1,129 +1,51 @@
+#importing libraries
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.signal import hilbert, chirp
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Assuming df is your dataframe read from csv
 def Hilbert(data, time):
-    x_values = []
+    """
+        Extracts amplitude envelope and instantaneous frequency from sensor data using the Hilbert transform.
+        simple explanation: It basically takes the instant frequency of a signal, So if a signal oscillates with
+        increasing speed, the graph of the hilbert transform will increase in time.
+
+        Parameters:
+        - data : A DataFrame where each column contains time-domain data.
+        - time (1D): The time values corresponding to the data.
+
+        Returns:
+        - DataFrame: A DataFrame containing the amplitude envelopes of the analytic signal for each data column.
+
+        Example:
+        result = Hilbert(sensor_data, time_values)
+        """
+    x_values = [] #check(not needed)
     time = pd.DataFrame(time)
-    inst_freq_arr = []
-    amp_arr = []
+    inst_freq_arr = [] #initialize array
+    amp_arr = [] #check(not needed)
+    # Iterate through sensors (Not 8 because of the actuator not being included)
     for i in range(7):
-        x_values = []
+        x_values = [] #check
         for j in range(8):
             k = 8 * i + j
             # Extracting data to perform FFT on
             x = data.iloc[:, k]
             time_intervals = np.diff(time)
-            #mean_interval = np.mean(time_intervals)
-            fs = 1 / 5e-7
+            fs = 1 / 5e-7 #sampling rate
 
-            analytic_signal = hilbert(x)
-            amplitude_envelope = np.abs(analytic_signal)
+            analytic_signal = hilbert(x) #apply Hilbert
+            amplitude_envelope = np.abs(analytic_signal) #Take the absolute value
             instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-            instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0 * np.pi) * fs)
+            instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0 * np.pi) * fs) #check(not needed)
 
-            # if statement tu cut off noise
-            # print(time[:-1])
-            # print(instantaneous_frequency)
-            #mask = np.logical_and(time[:-1] >= 0.0006, np.abs(instantaneous_frequency) >= 100000)
-            #instantaneous_frequency[mask] = 0
-
-
-            inst_freq_arr.insert(k, amplitude_envelope)
-    #print(pd.DataFrame(inst_freq_arr))
+            inst_freq_arr.insert(k, amplitude_envelope) #instant frequency array
     return pd.DataFrame(inst_freq_arr).transpose()
-    #
-    # for i in range(8):
-    #     data = df.iloc[:, i]
-    #
-    #     time_intervals = np.diff(time)
-    #     mean_interval = np.mean(time_intervals)
-    #     fs = 1 / 5e-7
-    #
-    #
-    #     analytic_signal = hilbert(data)
-    #     amplitude_envelope = np.abs(analytic_signal)
-    #     instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-    #     instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0 * np.pi) * fs)
-    #
-    #     # if statement tu cut off noise
-    #     mask = np.logical_and(time[:-1] >= 0.0006, np.abs(instantaneous_frequency) >= 100000)
-    #     instantaneous_frequency[mask] = 0
-    #
-        # save instantaneous freq
-        # fig, (ax0, ax1) = plt.subplots(nrows=2)
-        # ax0.plot(time, data, label='signal')
-        # ax0.plot(time, amplitude_envelope, label='envelope')
-        # ax0.set_xlabel("time in seconds")
-        # ax0.legend()
-        # ax1.plot(time[1:], instantaneous_frequency)
-        # ax1.set_xlabel("time in seconds")
-        # fig.tight_layout()
-        # plt.show()
-#GEORGE EDLYN HERE:)
-#the instantaneous frequency(after mask) is what needs to be saved
-#thus line 24
-
 
 def giveTime():
     time = []
     for i in range(2000):
         time.append(i*(5e-7))
     return pd.DataFrame(time)
-
-# def Hilbert(data, time):
-#     freq_arr = []
-#     amp_arr = []
-#     for i in range(8):
-#         for j in range(8):
-#             k = 8 * i + j
-#             x = data.iloc[:, k]
-#             fs = 1 / 5e-7
-#
-#             analytic_signal = hilbert(x)  # bruv rename the file
-#             amplitude_envelope = np.abs(analytic_signal)
-#             instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-#             instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0 * np.pi) * fs)
-#             # if statement tu cut off noise
-#             mask = np.logical_and(time[:-1] >= 0.0006, np.abs(instantaneous_frequency) >= 100000)
-#             instantaneous_frequency[mask] = 0
-#
-#             freq_arr.insert(k - 1, instantaneous_frequency)
-#             amp_arr.insert(k - 1, amplitude_envelope)
-#     return pd.DataFrame(freq_arr), pd.DataFrame(amp_arr)
-
-
-"Shape of passed values is (1999, 1999), indices imply (1999, 1) "
-
-"""
-for df in dataframes:
-    time = df.iloc[:, 0]
-    data = df.iloc[:, 1]
-
-    time_intervals = np.diff(time)
-    mean_interval = np.mean(time_intervals)
-    fs = 1 / mean_interval
-
-    analytic_signal = hilbert(data)
-    amplitude_envelope = np.abs(analytic_signal)
-    instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-    instantaneous_frequency = (np.diff(instantaneous_phase) /
-                               (2.0*np.pi) * fs)
-    # if statement tu cut off noise
-    mask = np.logical_and(time[:-1] >= 0.0006, np.abs(instantaneous_frequency) >= 100000)
-    instantaneous_frequency[mask] = 0
-
-    fig, (ax0, ax1) = plt.subplots(nrows=2)
-    ax0.plot(time, data, label='signal')
-    ax0.plot(time, amplitude_envelope, label='envelope')
-    ax0.set_xlabel("time in seconds")
-    ax0.legend()
-    ax1.plot(time[1:], instantaneous_frequency)
-    ax1.set_xlabel("time in seconds")
-    fig.tight_layout()
-    plt.show()
-"""
-
