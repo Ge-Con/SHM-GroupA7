0caa12ed37f0235fb8bfbdae7e764e3d09e8965b
twmkok <93989715+twmkok@users.noreply.github.com>
2024-06-13 04:50:30 +0200

    Update Ensemble_VAE.py

--- Ensemble_VAE.py
+++ Ensemble_VAE.py
@@ -12,6 +12,7 @@ from scipy.signal import resample_poly
 import math
 import scipy.interpolate as interp
 from skopt import gp_minimize
+from scipy.optimize import minimize
 from skopt.space import Real, Integer
 from skopt.utils import use_named_args
 import csv
@@ -148,7 +149,32 @@ def train_vae_ensemble(hidden_1, batch_size, learning_rate, epochs):
     print('Training finished!!!')
     end_time = time()
     print(f"Training time: {end_time - begin_time:.2f} seconds")
-    return sess, x, z
+    z_arr = sess.run(z, feed_dict={x: test})
+    z_arr = z_arr.transpose()
+    HI_arr = []
+    for j in tuple(x for x in panels if x != panel):
+        graph_data = pd.read_csv(dir_root + "\concatenated_" + freq + "_" + j + "_FFT_Features.csv", header=None).values.transpose()
+        graph_data = np.delete(graph_data, -1, axis=1)
+        graph_data = scaler.transform(graph_data)
+        graph_data = pca.transform(graph_data)
+        y_pred = sess.run(z, feed_dict={x: graph_data})
+        HI_arr.append(y_pred)
+    #scale all arrays to the same lenght
+    for i in range(len(HI_arr)):
+        HI_arr[i] = HI_arr[i].transpose()
+    max = find_largest_array_size(HI_arr)
+    for i in range(len(HI_arr)):
+        if HI_arr[i].size < max:
+            arr_interp = interp.interp1d(np.arange(HI_arr[i].size), HI_arr[i])
+            arr_stretch = arr_interp(np.linspace(0, HI_arr[i].size - 1, max))
+            HI_arr[i] = arr_stretch
+    HI_arr = np.vstack(HI_arr)
+    if z_arr.size != HI_arr.shape[1]:
+        arr_interp = interp.interp1d(np.arange(z_arr.size), z_arr)
+        arr_stretch = arr_interp(np.linspace(0, z_arr.size - 1, HI_arr.shape[1]))
+        z_arr = arr_stretch
+    full = np.append(HI_arr, z_arr, axis = 0)
+    return sess, x, z, full, HI_arr, z_arr
 
 def train_vae(hidden_1, batch_size, learning_rate, epochs):
     # Set hyperparameters and architecture details
@@ -274,9 +300,17 @@ def objective(**params):
     ftn, monotonicity, trendability, prognosability, error = fitness(train_vae(**params)[1])
     return error
 
+w_arr = [
+        Real(0.0001, 1, name='w0'),
+        Real(0.0001, 1, name='w1'),
+        Real(0.0001, 1, name='w2'),
+        Real(0.0001, 1, name='w3'),
+        Real(0.0001, 1, name="w4"),
+        Real(0.0001, 1, name='w5')]
+@use_named_args(w_arr)
 def objective_ensemble(**params):
     print(params)
-    ftn, monotonicity, trendability, prognosability, error = fitness(ensemble(**params))
+    ftn, monotonicity, trendability, prognosability, error = fitness(ensemble(**params)[1])
     return error
 
 def hyperparameter_optimisation(n_calls, random_state=42):
@@ -291,27 +325,24 @@ def ensemble(w0, w1, w2, w3, w4, w5):
     exists = False
     for i in range(len(models)):
         if not exists:
-            all_his = models[i][0].run(models[i][2], feed_dict={models[i][1]: data}).transpose()
+            all_test = np.expand_dims([models[i][5]], axis=2)
+            all_train = np.expand_dims([models[i][4]], axis=2)
+            all_full = np.expand_dims([models[i][3]], axis=2)
             exists = True
         else:
-            all_his = np.vstack((all_his, models[i][0].run(models[i][2], feed_dict={models[i][1]:data}).transpose()))
-    avg_z = np.average(all_his, axis=0, weights= [w0, w1, w2, w3, w4, w5])
-    return avg_z
+            all_test = np.concatenate((all_test, np.expand_dims([models[i][5]], axis=2)), axis =2)
+            all_train = np.concatenate((all_train,np.expand_dims([models[i][4]], axis=2)), axis =2)
+            all_full = np.concatenate((all_full, np.expand_dims([models[i][3]], axis=2)), axis =2)
+    avg_test = np.average(all_test, axis=2, weights= [w0, w1, w2, w3, w4, w5])
+    avg_train= np.average(all_train, axis=2, weights= [w0, w1, w2, w3, w4, w5])
+    avg_full = np.average(all_full, axis=2, weights= [w0, w1, w2, w3, w4, w5])
+    return avg_test[0, :, :], avg_train[0, :, :], avg_full[0, :, :]
 
-w_arr = [
-        Real(0.0001, 1, name='w0'),
-        Real(0.0001, 1, name='w1'),
-        Real(0.0001, 1, name='w2'),
-        Real(0.0001, 1, name='w3'),
-        Real(0.0001, 1, name="w4"),
-        Real(0.0001, 1, name='w5')]
-@use_named_args(w_arr)
 
-def optim2(n_calls):
-    res_gp = gp_minimize(objective_ensemble, w_arr, n_calls=n_calls, random_state=42,
-                         callback=[print_progress])
-    opt_parameters = res_gp.x
-    print("Best parameters found: ", res_gp.x)
+def optim2():
+    res = minimize(objective_ensemble, [0.15, 0.15, 0.15, 0.15, 0.15, 0.15])
+    opt_parameters = res.x
+    print("Best parameters found: ", res.x)
     return opt_parameters
 # You can create additional datasets if needed
 # Example: Using the first few columns as one dataset and the rest as another
@@ -322,15 +353,16 @@ panels = ("L103", "L105", "L109", "L104", "L123")
 freqs = ("050_kHz", "100_kHz", "125_kHz", "150_kHz", "200_kHz", "250_kHz")
 resdict = {}
 counter = 0
+hyperparameters_df = pd.read_csv(dir_root + '/hyperparameters-opt-FFT.csv', index_col=0)
 for panel in panels:
     models = []
     for freq in freqs:
         filenames = []
         for i in tuple(x for x in panels if x != panel):
-            filename = os.path.join(dir_root, f"concatenated_{freq}_{i}.csv")
+            filename = os.path.join(dir_root, f"concatenated_{freq}_{i}_FFT_Features.csv")
             filenames.append(filename)
         data, flags = mergedata(filenames)
-        test_filename = os.path.join(dir_root, f"concatenated_{freq}_{panel}.csv")
+        test_filename = os.path.join(dir_root, f"concatenated_{freq}_{panel}_FFT_Features.csv")
         test = pd.read_csv(test_filename, header=None).values.transpose()
         data.drop(data.columns[len(data.columns) - 1], axis=1, inplace=True)
         test = np.delete(test, -1, axis=1)
@@ -342,11 +374,28 @@ for panel in panels:
         pca.fit(data)
         data = pca.transform(data)
         test = pca.transform(test)
+        hyperparameters_str = hyperparameters_df.loc[freq, panel]
+        hyperparameters = eval(hyperparameters_str)
         # Set hyperparameters and architecture details
-        model = train_vae_ensemble(10, 16, 0.01, 1000)
+        model = train_vae_ensemble(hyperparameters[0][0], hyperparameters[0][1],
+                                   hyperparameters[0][2], hyperparameters[0][3])
         models.append(model)
-    print(ensemble(0.15, 0.15, 0.15, 0.15, 0.15, 0.15))
-    print(fitness(ensemble(0.15, 0.15, 0.15, 0.15, 0.15, 0.15)))
+    df = pd.DataFrame([[np.nan] * 6], columns=panels)
+    output = ensemble(0.15, 0.15, 0.15, 0.15, 0.15, 0.15)
+    df.loc[0, panel] = (test_fitness(output[0], output[1]), fitness(output[2]))
+#    parameters = optim2()
+#    for i, hi in enumerate(ensemble(*parameters)[2]):
+#        plt.plot(hi, label=f'HI_arr {i + 1}')
+#    plt.xlabel('Timesteps')
+#    plt.ylabel('Health Indicators')
+#    plt.title('Health Indicators over Time')
+#    plt.legend()
+#    plt.show()
+#    print(fitness(ensemble(*parameters)[2]))
+
+    for i in models:
+        i[0].close()
+df.to_csv("Ensemble_fitness_scores.csv")
 
 with open("results.csv", "w", newline="") as f:
     w = csv.DictWriter(f, resdict.keys())
