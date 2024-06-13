Weighted average and PCA fixed again again again

WAE.py
@@ -1,3 +1,39 @@
 import numpy as np
+import csv
+from prognosticcriteria_v2 import fitness
 
-print("Input:")
+def wae_fitness(HIs, num):
+    #Array of 6 HIs
+    w = np.zeros((num))
+    for run in range(num):
+        w[run] = fitness(HIs[run])[0]
+    w = w/np.sum(w)
+    newHIs = np.zeros((5, 30))
+    for run in range(num):
+        newHIs += w[run] * HIs[run]
+    return newHIs
+
+def sae_fitness(HIs):
+    newHI = np.sum(HIs/6)
+    return newHI
+
+print("Files must be 5D - 6 frequencies, n repetitions, 5 folds, 5 panels of 30 HIs")
+filepath = input("Input path to file: ")
+filename = input("Input HI filename:  ")
+
+HIs = np.load(filepath + "\\" + filename)
+print(HIs.shape)
+if HIs.ndim != 5:
+    print("Wrong number of dimensions")
+else:
+    for freq in range(6):
+        print("Freq " + str(freq))
+        newHIses = []
+        for fold in range(5):
+            print("-> fold " + str(fold))
+            newHIs = wae_fitness(HIs[freq, :, fold], 4) #Average repetitions
+            print(fitness(newHIs)[0])
+            newHIses.append(newHIs)
+        nnHI = wae_fitness(newHIses, 5)
+        print("Freq averaged:")
+        print(fitness(nnHI)[0])

main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import csv
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 
@@ -403,6 +404,14 @@ def save_evaluation(features, label, dir, files_used=[""]):  #Features is 6x fre
             This function saves the pca output for each campaign
     """
 
+    count = 1
+    savedir = dir + '\\' + label
+    while os.path.exists(savedir + '.npy'):
+        savedir = dir + '\\' + label + str(count)
+        count += 1
+    np.save(savedir, features)
+    print(features.shape)
+
     if features.ndim > 4:
         criteria = []
         for i in range(features.shape[1]):
@@ -567,7 +576,7 @@ repeat = True
 while repeat:
     #main_menu()
     #choice = input("Enter your choice: ")
-    choice = '9'
+    choice = '6'
     repeat = False
 
     if choice == '0':