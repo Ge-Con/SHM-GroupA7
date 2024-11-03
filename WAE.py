d6b7ca6a919b0966be87297488c4f47db1ac24dc
jjperryj <146219540+jjperryj@users.noreply.github.com>
2024-11-03 19:07:31 +0100

    Improved WAE
    Untested and still missing standard deviation

--- WAE.py
+++ WAE.py
@@ -46,28 +46,30 @@ def wae(HIs):
 
 print("Files must be 5D - 6 frequencies, n repetitions, 5 folds, 5 panels of 30 HIs")
 
-def eval_wae(filepath):
+def eval_wae(filepath, type):
     """
-        Execute and evaluate WAE HIs
-        This function was written shortly before the TAS deadline so may contain errors, in addition to the following:
-        - The function must be manually changed between producing results for FFT and Hilbert transform
-        - Standard deviation for AI methods could not be implemented due to numpy errors.
-        The hurried writing was also the source for often poor choice in variable names.
+        Execute and evaluate mean HIs and weighted average ensemble learning for HIs
+        Standard deviation not yet implemented due to numpy errors
 
         Parameters:
-        - merge (bool): Whether to perform WAE across folds
+        - filepath (string): directory of HI CSVs
+        - type (string): "FFT" or "HLB"
 
         Returns: None
     """
 
     # Seeds used
     files = ["140"]#["42", "110", "120", "130", "140"]    # 5 seeds for VAE, 3 for DeepSAD due to time
+    print("Ensure .npy filenames end with '_n', where n is from 1 to the number of seeds used")
+    print("\te.g. DeepSAD_FFT_1.npy")
+    print("\tThis should match the number of seeds given in WAE.py")
+    print("\tNumber of seeds given: " + str(len(files)))
 
     # Simple averaging repetitions
-    storeHIs = np.empty((3), dtype=object)
-    for repetition in range(3):
+    storeHIs = np.empty((len(files)), dtype=object)
+    for repetition in range(len(files)):
         print("Repetition " + str(repetition+1))
-        filename = "DF" + files[repetition] + ".npy"
+        filename = "DeepSAD_" + type + "_" + files[repetition] + ".npy"
         storeHIs[repetition] = np.load(filepath + "\\" + filename, allow_pickle=True)
 
     # Define arrays for mean and standard deviation of HIs and fitness scores
@@ -83,7 +85,7 @@ def eval_wae(filepath):
             #stdfit[freq, fold] = meanfit[freq][fold] - fitness(meanHIs[freq][fold]+stdHIs[freq][fold])[0]
 
     #Save to CSVs
-    pd.DataFrame(meanfit).to_csv(os.path.join(filepath, "meanfitFFT.csv"), index=False)
+    pd.DataFrame(meanfit).to_csv(os.path.join(filepath, "meanfit" + type + ".csv"), index=False)
     #pd.DataFrame(meanfit).to_csv(os.path.join(dir, "stdfit.csv"), index=False)
 
     # Repeat for F-test scores
@@ -95,7 +97,7 @@ def eval_wae(filepath):
         for freq in range(6):
             tmeanfit[freq, fold] = test_fitness(meanHIs[freq][fold][fold], meanHIs[freq][fold])
             #tstdfit[freq, fold] = tmeanfit[freq, fold] - test_fitness(meanHIs[freq, fold, fold] + stdHIs[freq, fold, fold], meanHIs[freq, fold] + stdHIs[freq, fold])[0]
-    pd.DataFrame(tmeanfit).to_csv(os.path.join(filepath, "tmeanfitFFT.csv"), index=False)
+    pd.DataFrame(tmeanfit).to_csv(os.path.join(filepath, "test-meanfit" + type + ".csv"), index=False)
     #pd.DataFrame(meanfit).to_csv(os.path.join(dir, "tstdfit.csv"), index=False)
 
     # Carry out and save WAE between folds for F-all
@@ -104,7 +106,7 @@ def eval_wae(filepath):
         print("-> Fold " + str(fold))
         newHIs = wae(meanHIs[:][fold])  # Average repetitions
         newHIses.append(fitness(newHIs)[0])
-    pd.DataFrame(newHIses).to_csv(os.path.join(filepath, "weightedFFT.csv"), index=False)
+    pd.DataFrame(newHIses).to_csv(os.path.join(filepath, "weighted" + type + ".csv"), index=False)
 
     # Carry out and save WAE between folds for F-test
     newHIses = []
@@ -112,4 +114,4 @@ def eval_wae(filepath):
         print("-> Fold " + str(fold))
         newHIs = wae(meanHIs[:][fold])  # Average repetitions
         newHIses.append(test_fitness(newHIs[fold], newHIs))
-    pd.DataFrame(newHIses).to_csv(os.path.join(filepath, "tweightedFFT.csv"), index=False)
+    pd.DataFrame(newHIses).to_csv(os.path.join(filepath, "test-weighted" + type + ".csv"), index=False)
